353207519c488549bf9011ef104c91e6e97a4500
Jiri <jiriplic@seznam.cz>
2020-11-10 16:09:34 +0100

    portfolio react web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import Navigation from "./component/Navigation/Navigation";
+import Logo from "./component/Logo/Logo";
+import ImageLinkForm from "./component/ImageLinkForm/ImageLinkForm";
+import Rank from "./component/Rank/Rank";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions ={
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
 }
 
+class App extends Component {
+  render() {
+    return ( 
+      <div className = "App">
+      <Particles className="particles"
+        params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        
+      </div>
+    );
+}
+}
 export default App;
